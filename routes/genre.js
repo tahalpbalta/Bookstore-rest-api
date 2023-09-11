6634b4311d0502883f60addf77cb8e45bd903a41
tahalpbalta <tahalpbalta@gmail.com>
2023-09-11 19:14:27 +0300

    auth

--- routes/genre.js
+++ routes/genre.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const Book = require('../model/books');
-const authenticateToken = require('../middleware/authenticate');
+//const authenticateToken = require('../middleware/authenticate');
 
-router.use(authenticateToken);
+//router.use(authenticateToken);
 
-router.get('/tur/:genre', authenticateToken, async (req,res,next) => {
+router.get('/tur/:genre', async (req,res,next) => {
     try {
         const genre = req.params.genre.toLowerCase();
         const booksByGenre = await Book.find({genre});
